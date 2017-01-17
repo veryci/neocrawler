b465525b535dacb2a3d3ebebc2e48aa47da55eae
sankooc <sankooc@163.com>
2017-01-17 14:09:25 +0800

    add babytree

--- tool/index.js
+++ tool/index.js
@@ -3,7 +3,9 @@ exports.load = (settings) => {
   global.settings = settings;
   const instance = settings.instance;
   const rules = require(`../instance/${instance}/rules`);
-  rules.forEach((jsonobj) => {
+  const keys = Object.keys(rules);
+  keys.forEach((key) => {
+    const jsonobj = rules[key];
     jsonobj.instance = instance;
     redis.drillerInfoRedis.set(`driller:${instance}:${jsonobj.domain}:${jsonobj.alias}`, JSON.stringify(jsonobj));
   });
